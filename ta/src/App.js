9af31932738603dbe0a308f74b5a8644acedc834
NicPell <pellicena@gmail.com>
2018-10-17 16:29:05 +0100

    Fetch mentors

--- ta/src/App.js
+++ ta/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import { BrowserRouter as Router, Route } from "react-router-dom"
 
 import "./App.scss";
 import Subscription from "./subscription";
--- ta/src/components/Profile.js
+++ ta/src/components/Profile.js
@@ -1,4 +1,11 @@
 import React, { Component } from 'react'
+import fetch from 'isomorphic-fetch'
+
+function fetchMentors() {
+  fetch('http://localhost:3000/mentors')
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
 
 class Profile extends Component {
   render() {
@@ -6,6 +13,7 @@ class Profile extends Component {
       <div>
         <h4>Hermione Granger</h4>
         <h4>Your Matches</h4>
+        {fetchMentors()}
       </div>
     )
   }
