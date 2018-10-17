d0eaccb154724dbc92669474543fa8d5f6a66939
NicPell <pellicena@gmail.com>
2018-10-17 17:48:02 +0100

    Display mentors

--- ta/src/components/Profile.js
+++ ta/src/components/Profile.js
@@ -1,19 +1,37 @@
 import React, { Component } from 'react'
-import fetch from 'isomorphic-fetch'
-
-function fetchMentors() {
-  fetch('http://localhost:3000/mentors')
-    .then(response => response.json())
-    .then(json => console.log(json))
-}
+// import fetch from 'isomorphic-fetch'
+import axios from 'axios'
 
 class Profile extends Component {
+  state = {
+    mentors: []
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3000/mentors')
+      .then(response => response.json())
+      .then(data => this.setState({ mentors: data }))
+  }
+
   render() {
     return (
       <div>
         <h4>Hermione Granger</h4>
         <h4>Your Matches</h4>
-        {fetchMentors()}
+        {this.state.mentors.map(mentor => {
+          return (
+            <li key={mentor.id}>
+              {mentor.name}:
+              {mentor.expertises.map(expertise => {
+                return (
+                  <ul>
+                    <li>{expertise}</li>
+                  </ul>
+                )
+              })}
+            </li>
+          )
+        })}
       </div>
     )
   }
