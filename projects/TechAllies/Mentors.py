108c183bb82e0138676df3c178ec0b9529dd72da
Eimi Okuno <emettely@users.noreply.github.com>
2018-10-17 18:21:32 +0100

    Backend Flask app (#3)
    * Committing server.py and Mentors.py
    
    * gitignore for compiled fiels
    
    * Please fix this :(
    
    * Undo list
    
    * It works

---
+++ projects/TechAllies/Mentors.py
@@ -0,0 +1,59 @@
+from faker import Faker
+
+fake = Faker()
+
+expertise_area = ["Data Science",
+                  "Backend Engineer",
+                  "Frontend Engineer",
+                  "Full-stack Engineer",
+                  "Software Architect"]
+
+gender = ['Female', 'Male', 'Other']
+
+
+class Mentee():
+    def __init__(self, name: str, criterion: dict, gender=None):
+        self.name = name
+        self.criterion = criterion
+        self.gender = gender
+
+    def get_criterion(self):
+        return self.criterion
+
+
+class Mentor():
+    def __init__(self, name: str, expertises: set, meet: set,
+                 location={}, gender=''):
+        self.name = name
+        self.expertises = list(expertises)
+        self.meet = list(meet)
+        self.location = list(location)
+        self.gender = gender
+
+        if not name:
+            raise AttributeError()
+
+    def __repr__(self):
+        return str(self.__dict__)
+
+    def has_matching_criteria(self, cKey: str, cVal) -> list:
+        if hasattr(self, cKey):
+            # gender is a String
+            if cKey == "gender":
+                if cVal == self.gender:
+                    return [cVal]
+            elif cKey == "expertises":
+                match = [c for c in cVal if c in self.expertises]
+                return match
+            else:
+                match = [c for c in cVal if c in self.__getattribute__(cKey)]
+                return match
+        return []
+
+
+def gen_Mentors():
+    yield Mentor(fake.name(),
+                 expertises={fake.random.choice(expertise_area)},
+                 meet={'offline'},
+                 location={'Manchester'},
+                 gender=fake.random.choice(gender))
