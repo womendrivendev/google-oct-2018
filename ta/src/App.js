4253522275e02724a1f9b61f5b7d0311cd92c591
NicPell <pellicena@gmail.com>
2018-10-17 16:13:07 +0100

    Add react router

--- ta/src/App.js
+++ ta/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+
 import "./App.scss";
 import Subscription from "./subscription";
 import Profile from "./components/Profile";
@@ -7,12 +9,16 @@ import Header from "./components/Header";
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <h1>Tech Allies App</h1>
-        <Subscription />
-        <Profile />
-      </div>
+      <Router>
+        <div className="App">
+          <Header />
+          <div>
+            <Route exact path="/profile" component={Profile} />
+            <Route exact path="/subscribe" component={Subscription} />
+            <Route exact path="/" component={Subscription} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
--- ta/src/components/Header.js
+++ ta/src/components/Header.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import { Link } from "react-router-dom"
 
 class Header extends Component {
   render() {
     return (
       <div>
-        <h4>Header</h4>
+        <h1>Tech Allies App</h1>
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/subscribe">Subscribe</Link></li>
+            <li><Link to="/profile">Profile</Link></li>
+          </ul>
       </div>
     )
   }
