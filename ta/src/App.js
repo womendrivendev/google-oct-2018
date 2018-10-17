08cae05bef6fb77b2257448f9acba6c0b3ee0d40
Alicia Grandjean <alicia.grandjean@bbc.co.uk>
2018-10-17 14:53:23 +0100

    add Sass to the project

--- ta/src/App.js
+++ ta/src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 import Subscription from "./subscription";
 
 class App extends Component {
